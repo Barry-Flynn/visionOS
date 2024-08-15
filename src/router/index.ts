d51b70a3fc61e9ae3c204bf9303dbc5c538b83ce
百里飞洋 <Barry-Flynn@qq.com>
2024-08-16 04:09:55 +0800

    Switch vue-router from history mode to hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter } from 'vue-router'
+// import { createWebHistory } from 'vue-router'
+import { createWebHashHistory } from 'vue-router'
 import AppsView from '@/views/apps/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       // Redirect to the first child route
